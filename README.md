303662e702e7ef7ddd0dd7ac743a9cd494b592aa
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-08-09 02:17:08 +0300

    First try build

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # test_ipc_mutex_winapi
+
+[![Build status](https://ci.appveyor.com/api/projects/status/e70aax79r6039t5j?svg=true)](https://ci.appveyor.com/project/dvetutnev/test-ipc-mutex-winapi)
---
+++ simple.cpp
@@ -0,0 +1,15 @@
+// Test IPC mutex WinAPI
+// Simple test
+// Dmitriy Vetutnev, 2018
+
+
+#include <iostream>
+#include <cstdlib>
+
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "Hello world!" << std::endl;
+
+    return EXIT_SUCCESS;
+}
